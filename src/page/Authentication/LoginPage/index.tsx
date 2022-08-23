dd157b6dd04615900140a065f35435130b5871ac
Fahad Hussain <fahd__hussain@hotmail.com>
2022-08-24 00:40:21 +0500

    Login refactored

--- src/page/Authentication/LoginPage/index.tsx
+++ src/page/Authentication/LoginPage/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from "react";
+import { FC, FormEvent, KeyboardEvent, useEffect } from "react";
 import {
   Box,
   Button,
@@ -45,89 +45,98 @@ const LoginPage: FC<{}> = () => {
     }
   };
 
+  const _handleEnterKeyDown = (
+    event: KeyboardEvent<HTMLDivElement>,
+    handleSubmit: (e?: FormEvent<HTMLFormElement>) => void
+  ) => {
+    const { key, shiftKey, altKey, ctrlKey } = event;
+
+    if (key === "Enter" && !shiftKey && !altKey && !ctrlKey) {
+      handleSubmit();
+    }
+  };
+
   return (
     <Box className="_login_container">
-      <Box className="_left_content" />
-      <Box className="_login_form">
-        <Box />
-        <Box className="_login_form_container">
-          <Typography className="_login_form_header">Login</Typography>
-          <Formik
-            initialValues={initialValues}
-            validationSchema={loginValidationSchema}
-            onSubmit={_handleSubmit}
-          >
-            {({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleBlur,
-              handleSubmit,
-            }) => (
-              <Form>
-                <Box className="_login_form_body">
-                  <TextField
-                    className="_login_form_body_input"
-                    name="email"
-                    label="Email"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                    error={Boolean(errors.email && touched.email)}
-                    helperText={errors.email && touched.email}
-                    autoFocus
-                    variant="filled"
+      <Box className="_login_form_container">
+        <Typography className="_login_form_header">Login</Typography>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={loginValidationSchema}
+          onSubmit={_handleSubmit}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+          }) => (
+            <Form>
+              <Box className="_login_form_body">
+                <TextField
+                  className="_login_form_body_input"
+                  name="email"
+                  label="Email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                  error={Boolean(errors.email && touched.email)}
+                  helperText={errors.email && touched.email}
+                  autoFocus
+                  variant="filled"
+                />
+                <TextField
+                  className="_login_form_body_input"
+                  name="password"
+                  label="Password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                  error={Boolean(errors.password && touched.password)}
+                  helperText={errors.password && touched.password}
+                  type="passeord"
+                  variant="filled"
+                  onKeyDownCapture={(event) =>
+                    _handleEnterKeyDown(event, handleSubmit)
+                  }
+                />
+                <Box className="_Login_form_body_submit_remember">
+                  <FormControlLabel
+                    value="start"
+                    control={<Checkbox />}
+                    label="Remember me"
+                    labelPlacement="start"
+                    className="_Login_form_body_remember_chk"
                   />
-                  <TextField
-                    className="_login_form_body_input"
-                    name="password"
-                    label="Password"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.password}
-                    error={Boolean(errors.password && touched.password)}
-                    helperText={errors.password && touched.password}
-                    type="passeord"
-                    variant="filled"
-                  />
-                  <Box className="_Login_form_body_submit_remember">
-                    <FormControlLabel
-                      value="start"
-                      control={<Checkbox />}
-                      label="Remember me"
-                      labelPlacement="start"
-                      className="_Login_form_body_remember_chk"
-                    />
-                    <Button
-                      variant="outlined"
-                      className="_Login_form_body_submit_btn"
-                      onClick={() => handleSubmit()}
-                    >
-                      Submit
-                    </Button>
-                  </Box>
+                  <Button
+                    variant="outlined"
+                    className="_Login_form_body_submit_btn"
+                    onClick={() => handleSubmit()}
+                  >
+                    Submit
+                  </Button>
                 </Box>
-              </Form>
-            )}
-          </Formik>
-          <Box className="_login_form_footer">
-            <Typography>
-              Forgot
-              <Link className="_login_form_footer_link" to="/register">
-                password
-              </Link>
-            </Typography>
-            <Typography>
-              Dont have an Account? Create one from
-              <Link className="_login_form_footer_link" to="/register">
-                here
-              </Link>
-            </Typography>
-          </Box>
+              </Box>
+            </Form>
+          )}
+        </Formik>
+        <Box className="_login_form_footer">
+          <Typography>
+            Forgot
+            <Link className="_login_form_footer_link" to="/register">
+              password
+            </Link>
+          </Typography>
+          <Typography>
+            Dont have an Account? Create one from
+            <Link className="_login_form_footer_link" to="/register">
+              here
+            </Link>
+          </Typography>
         </Box>
       </Box>
-      <Box className="_right_content" />
     </Box>
   );
 };
